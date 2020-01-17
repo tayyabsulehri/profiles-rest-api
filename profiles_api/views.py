90d96a1bd3d081916daa1278522a542247b30071
Tayyab G <tayyabsulehri@gmail.com>
2020-01-17 15:17:45 +0400

    added search functionality

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from profiles_api import Serializers
 from rest_framework import viewsets
+from rest_framework.authtoken.views import obtain_auth_token, ObtainAuthToken
+from rest_framework.settings import api_settings
+
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
@@ -84,3 +87,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle create suer authentication token"""
+
+    renderer_classes= api_settings.DEFAULT_RENDERER_CLASSES
